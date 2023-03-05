1eaa0f719780cb7f39568a6ffbdd2f3819cde746
Eugene Butusov <evgenybutusov@gmail.com>
2023-03-05 18:30:02 +0300

    Enable OpenAPI

--- apps/kickstart/src/app/app.controller.ts
+++ apps/kickstart/src/app/app.controller.ts
@@ -6,8 +6,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getData() {
+  @Get('/welcome')
+  welcome() {
     return this.appService.getData();
   }
 }
--- apps/kickstart/src/main.ts
+++ apps/kickstart/src/main.ts
@@ -7,11 +7,20 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  // set up OpenAPI
+  const config = new DocumentBuilder()
+    .setTitle('Kickstart API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(globalPrefix, app, document);
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
