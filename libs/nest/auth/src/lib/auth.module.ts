30e05f02520f9073dd1d2c6039f83ad03fda0e10
Eugene Butusov <evgenybutusov@gmail.com>
2023-03-07 11:26:27 +0300

    Add passport dependencies

--- libs/nest/auth/src/lib/auth.module.ts
+++ libs/nest/auth/src/lib/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from '../strategies/local.strategy';
 import { JwtStrategy } from '../strategies/jwt.strategy';
+import { AuthController } from './auth.controller';
 
 @Module({
-  controllers: [],
+  controllers: [AuthController],
   providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [],
 })
