c7d945c4ef547d304ef87e88a03ca8a091eba119
Eugene Butusov <evgenybutusov@gmail.com>
2023-03-07 15:06:23 +0300

    Add passport dependencies

--- apps/kickstart/src/app/app.module.ts
+++ apps/kickstart/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from '@libs/nest/auth/lib/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- libs/nest/auth/src/lib/auth.controller.ts
+++ libs/nest/auth/src/lib/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger, Post } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  private readonly logger = new Logger(AuthController.name);
+
+  @Post('token')
+  async token() {
+    return { message: 'authorized' };
+  }
+}
