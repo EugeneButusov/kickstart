eb815983a0482382075ecf5a84d7224293847446
Eugene Butusov <evgenybutusov@gmail.com>
2023-03-07 15:24:41 +0300

    Add passport dependencies

--- libs/nest/auth/src/lib/auth.controller.ts
+++ libs/nest/auth/src/lib/auth.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Logger, Post } from '@nestjs/common';
+import { Controller, Logger, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
   private readonly logger = new Logger(AuthController.name);
 
+  @UseGuards(AuthGuard('local'))
   @Post('token')
   async token() {
     return { message: 'authorized' };
