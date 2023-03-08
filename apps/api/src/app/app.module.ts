8c3ab5d0b0b4f05f49dc06abbe4f8ca867e4f928
Eugene Butusov <evgenybutusov@gmail.com>
2023-03-08 16:40:28 +0300

    Update README.md

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,9 +3,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from '@libs/nest/users/lib/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    // TODO: typeorm should be extracted as config
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: 'localhost',
+      port: 27017,
+      database: 'default',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- libs/nest/users/src/lib/users.module.ts
+++ libs/nest/users/src/lib/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
