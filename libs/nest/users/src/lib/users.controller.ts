9d8135083da85e3437e349653c2d9ad5e3645d9d
Eugene Butusov <evgenybutusov@gmail.com>
2023-03-08 16:25:31 +0300

    Intermediate commit

--- libs/nest/users/src/lib/users.controller.ts
+++ libs/nest/users/src/lib/users.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() payload: unknown): Promise<unknown> {
+    return { message: 'user created', data: payload };
+  }
+
+  @Get('/:id')
+  async get(@Param('id') id: string): Promise<unknown> {
+    return { message: `user retrieved with ID ${id}` };
+  }
 }
