655c2e440de772a734e3b7d125af6505b399caf0
Eugene Butusov <evgenybutusov@gmail.com>
2023-03-09 11:16:21 +0300

    Intermediate commit

--- libs/nest/users/src/entities/user.entity.ts
+++ libs/nest/users/src/entities/user.entity.ts
@@ -1,19 +1,15 @@
-import { Column, Entity, ObjectIdColumn, PrimaryColumn } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 import { User } from '../interfaces/user.interface';
+import { ObjectIdColumn } from '@libs/nest/common/decorators/object-id-column.decorator';
 
 @Entity('users')
 export class UserEntity implements User {
-  @ObjectIdColumn()
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  _id: string;
-
-  @PrimaryColumn()
+  @ObjectIdColumn({ name: '_id' })
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   id: string;
 
-  @Column()
+  @Column({ unique: true })
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   username: string;
