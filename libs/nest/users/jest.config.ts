fc2eec7363714068667a086522c280a41ef9cf76
Eugene Butusov <evgenybutusov@gmail.com>
2023-03-11 11:58:45 +0300

    Intermediate commit

--- libs/nest/users/jest.config.ts
+++ libs/nest/users/jest.config.ts
@@ -6,7 +6,6 @@ export default {
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
-  testMatch: ['**/+(*.|*.e2e-)+(spec|test).+(ts|js)?(x)'],
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../../coverage/libs/nest/users',
 };
