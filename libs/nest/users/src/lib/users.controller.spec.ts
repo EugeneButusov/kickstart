c533d8e760926de0d8f8e21103def3b6c66294b4
Eugene Butusov <evgenybutusov@gmail.com>
2023-03-11 18:20:08 +0300

    Intermediate commit

--- libs/nest/users/src/lib/users.controller.spec.ts
+++ libs/nest/users/src/lib/users.controller.spec.ts
@@ -1,22 +1,21 @@
 import { Test } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { UserEntity } from '../entities/user.entity';
+
+const usersServiceMock = {
+  create: jest.fn(),
+  findById: jest.fn(),
+  find: jest.fn(),
+  updateById: jest.fn(),
+  deleteById: jest.fn(),
+};
 
 describe('UsersController', () => {
-  const usersRepositoryMock = {};
   let controller: UsersController;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [
-        {
-          provide: getRepositoryToken(UserEntity),
-          useValue: usersRepositoryMock,
-        },
-        UsersService,
-      ],
+      providers: [{ provide: UsersService, useValue: usersServiceMock }],
       controllers: [UsersController],
     }).compile();
 
