6fe481edcb7e218a1a43f4a23f18fbc65f53fc92
Eugene Butusov <evgenybutusov@gmail.com>
2023-03-11 18:10:54 +0300

    Intermediate commit

--- libs/nest/users/src/lib/users.service.spec.ts
+++ libs/nest/users/src/lib/users.service.spec.ts
@@ -3,8 +3,23 @@ import { UsersService } from './users.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { UserEntity } from '../entities/user.entity';
 
+// TODO: implement factory for fixtures
+const userFixture = (seed = '1') => ({ username: `username-${seed}` });
+
+const usersRepositoryMock = {
+  create: jest.fn().mockImplementation(() => ({ ...userFixture() })),
+  save: jest
+    .fn()
+    .mockImplementation((entity) => ({ ...entity, id: 'test-id' })),
+  findOneById: jest.fn().mockImplementation((id) => ({ ...userFixture(), id })),
+  findBy: jest
+    .fn()
+    .mockImplementation(() => [{ ...userFixture(), id: 'test-id' }]),
+  update: jest.fn().mockImplementation(() => ({ affected: 1 })),
+  delete: jest.fn().mockImplementation(() => ({ affected: 1 })),
+};
+
 describe('UsersService', () => {
-  const usersRepositoryMock = {};
   let service: UsersService;
 
   beforeEach(async () => {
@@ -23,5 +38,67 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('#create', () => {
+    const user = userFixture();
+
+    describe('happy path', () => {
+      it('should resolve', () =>
+        expect(service.create(user)).resolves.toMatchInlineSnapshot(`
+          {
+            "id": "test-id",
+            "username": "username-1",
+          }
+        `));
+    });
+  });
+
+  describe('#findById', () => {
+    const userId = 'test-id';
+
+    describe('happy path', () => {
+      it('should resolve', () =>
+        expect(service.findById(userId)).resolves.toMatchInlineSnapshot(`
+          {
+            "id": "test-id",
+            "username": "username-1",
+          }
+        `));
+    });
+  });
+
+  describe('#find', () => {
+    describe('happy path', () => {
+      it('should resolve', () =>
+        expect(service.find()).resolves.toMatchInlineSnapshot(`
+          [
+            {
+              "id": "test-id",
+              "username": "username-1",
+            },
+          ]
+        `));
+    });
+  });
+
+  describe('#updateById', () => {
+    const userId = 'test-id';
+
+    describe('happy path', () => {
+      it('should resolve', () =>
+        expect(service.updateById(userId, userFixture('2'))).resolves.toBe(
+          true
+        ));
+    });
+  });
+
+  describe('#deleteById', () => {
+    const userId = 'test-id';
+
+    describe('happy path', () => {
+      it('should resolve', () =>
+        expect(service.deleteById(userId)).resolves.toBe(true));
+    });
   });
 });
