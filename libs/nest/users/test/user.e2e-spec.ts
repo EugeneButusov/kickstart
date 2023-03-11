ea17775fde00d5c17666b1b64e4e927fd936f891
Eugene Butusov <evgenybutusov@gmail.com>
2023-03-11 11:57:45 +0300

    Intermediate commit

---
+++ libs/nest/users/test/user.e2e-spec.ts
@@ -0,0 +1,24 @@
+import { Test } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { UsersModule, UsersService } from '../src';
+
+describe('Users (e2e)', () => {
+  let app: INestApplication;
+  const usersService = {};
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [UsersModule],
+    })
+      .overrideProvider(UsersService)
+      .useValue(usersService)
+      .compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
