5ed67120fc06ab7cef357bd39c12b5929a19716b
Eugene Butusov <evgenybutusov@gmail.com>
2023-03-11 18:24:33 +0300

    Intermediate commit

--- libs/nest/users/src/lib/users.controller.spec.ts
+++ libs/nest/users/src/lib/users.controller.spec.ts
@@ -1,13 +1,22 @@
 import { Test } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import * as _ from 'lodash';
+
+// TODO: implement factory for fixtures
+const userFixture = (seed = '1') => ({
+  id: `id-${seed}`,
+  username: `username-${seed}`,
+});
 
 const usersServiceMock = {
-  create: jest.fn(),
-  findById: jest.fn(),
-  find: jest.fn(),
-  updateById: jest.fn(),
-  deleteById: jest.fn(),
+  create: jest
+    .fn()
+    .mockImplementation((payload) => ({ ...payload, ...userFixture() })),
+  findById: jest.fn().mockImplementation((id) => ({ ...userFixture(), id })),
+  find: jest.fn().mockImplementation(() => [{ ...userFixture() }]),
+  updateById: jest.fn().mockImplementation(() => ({ affected: 1 })),
+  deleteById: jest.fn().mockImplementation(() => ({ affected: 1 })),
 };
 
 describe('UsersController', () => {
@@ -26,7 +35,20 @@ describe('UsersController', () => {
     expect(controller).toBeTruthy();
   });
 
-  xdescribe('#create', () => {});
+  describe('#create', () => {
+    const userCreateParams = _.omit(userFixture(), 'id');
+
+    describe('happy path', () => {
+      it('should resolve with user', () =>
+        expect(controller.create(userCreateParams)).resolves
+          .toMatchInlineSnapshot(`
+          {
+            "id": "id-1",
+            "username": "username-1",
+          }
+        `));
+    });
+  });
 
   xdescribe('#get', () => {});
 
