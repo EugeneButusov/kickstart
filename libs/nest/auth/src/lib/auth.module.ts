9808b45390cb0479e97f56302f1f10fdb6d82d8c
Eugene Butusov <evgenybutusov@gmail.com>
2023-03-12 18:35:34 +0300

    Fix CI

--- libs/nest/auth/src/lib/auth.module.ts
+++ libs/nest/auth/src/lib/auth.module.ts
@@ -3,8 +3,15 @@ import { AuthService } from './auth.service';
 import { LocalStrategy } from '../strategies/local.strategy';
 import { JwtStrategy } from '../strategies/jwt.strategy';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: 'jwt-secret', // TODO: extract in configuration
+      signOptions: { expiresIn: '60s' }, // TODO: extract in configuration
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [],
