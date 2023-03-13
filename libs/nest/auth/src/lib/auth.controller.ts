79b3f1e0842eedc37cf0861c08c78ca0e538e9c2
Eugene Butusov <evgenybutusov@gmail.com>
2023-03-13 10:37:29 +0300

    Intermediate commit

--- libs/nest/auth/src/lib/auth.controller.ts
+++ libs/nest/auth/src/lib/auth.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Logger, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('auth')
+@ApiTags('auth')
 export class AuthController {
   private readonly logger = new Logger(AuthController.name);
 
