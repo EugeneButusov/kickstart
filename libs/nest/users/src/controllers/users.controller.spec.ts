582a092d45d25e805709e5a69d2b0c691324b2d3
Eugene Butusov <evgenybutusov@gmail.com>
2023-03-14 16:47:24 +0300

    Intermediate commit

--- libs/nest/users/src/controllers/users.controller.spec.ts
+++ libs/nest/users/src/controllers/users.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test } from '@nestjs/testing';
-import { UsersController } from '../controllers/users.controller';
-import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { UsersService } from '../lib/users.service';
 import * as _ from 'lodash';
 
 // TODO: implement factory for fixtures
