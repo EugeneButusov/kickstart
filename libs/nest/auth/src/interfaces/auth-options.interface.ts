e34bfa9960d22583c804499280ade160975ac508
Eugene Butusov <evgenybutusov@gmail.com>
2023-03-14 15:10:26 +0300

    Intermediate commit

--- libs/nest/auth/src/interfaces/auth-options.interface.ts
+++ libs/nest/auth/src/interfaces/auth-options.interface.ts
@@ -1,4 +1,3 @@
-import { ModuleMetadata, Provider, Type } from '@nestjs/common';
 import { JwtModuleOptions } from '@nestjs/jwt/dist/interfaces/jwt-module-options.interface';
 
 export type AuthModuleOptions = {
@@ -7,16 +6,4 @@ export type AuthModuleOptions = {
 
 export interface AuthOptionsFactory {
   createAuthOptions(): Promise<AuthModuleOptions> | AuthModuleOptions;
-}
-
-export interface AuthModuleAsyncOptions
-  extends Pick<ModuleMetadata, 'imports'> {
-  name?: string;
-  useExisting?: Type<AuthOptionsFactory>;
-  useClass?: Type<AuthOptionsFactory>;
-  useFactory?: (
-    ...args: any[]
-  ) => Promise<AuthModuleOptions> | AuthModuleOptions;
-  inject?: any[];
-  extraProviders?: Provider[];
 }
