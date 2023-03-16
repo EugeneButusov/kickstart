3bb9614b04082f617cd4a5ace14a79719ffe6667
Eugene Butusov <evgenybutusov@gmail.com>
2023-03-16 16:52:11 +0300

    Intermediate commit

--- libs/nest/users/src/controllers/admin.users.controller.spec.ts
+++ libs/nest/users/src/controllers/admin.users.controller.spec.ts
@@ -4,9 +4,10 @@ import { userFixture } from '../../test/fixtures/user.fixture';
 import { AdminUsersController } from './admin.users.controller';
 
 const usersServiceMock = {
-  create: jest
-    .fn()
-    .mockImplementation((payload) => ({ ...payload, ...userFixture() })),
+  findById: jest.fn().mockImplementation((id) => ({ ...userFixture(), id })),
+  find: jest.fn().mockImplementation(() => [{ ...userFixture() }]),
+  updateById: jest.fn().mockImplementation(() => true),
+  deleteById: jest.fn().mockImplementation(() => true),
 };
 
 describe('AdminUsersController', () => {
@@ -23,5 +24,97 @@ describe('AdminUsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeTruthy();
+  });
+
+  describe('#get', () => {
+    const userId = 'test-id';
+
+    describe('happy path', () => {
+      it('should resolve to entity', () =>
+        expect(controller.get(userId)).resolves.toMatchInlineSnapshot(`
+          UserGetDto {
+            "id": "test-id",
+            "role": "regular",
+            "username": "username-1",
+          }
+        `));
+    });
+
+    describe('when nothing found', () => {
+      beforeAll(() => {
+        usersServiceMock.findById.mockResolvedValueOnce(null);
+      });
+
+      it('should reject', () =>
+        expect(
+          controller.get(userId)
+        ).rejects.toThrowErrorMatchingInlineSnapshot(`"User not found"`));
+    });
+  });
+
+  describe('#list', () => {
+    describe('happy path', () => {
+      it('should resolve to array', () =>
+        expect(controller.list()).resolves.toMatchInlineSnapshot(`
+          [
+            {
+              "id": "id-1",
+              "role": "regular",
+              "username": "username-1",
+            },
+          ]
+        `));
+    });
+
+    describe('when nothing found', () => {
+      beforeAll(() => {
+        usersServiceMock.find.mockResolvedValueOnce([]);
+      });
+
+      it('should resolve to an empty array', () =>
+        expect(controller.list()).resolves.toMatchInlineSnapshot(`[]`));
+    });
+  });
+
+  describe('#update', () => {
+    const userId = 'test-id';
+
+    describe('happy path', () => {
+      it('should resolve', () =>
+        expect(controller.update(userId, userFixture())).resolves.toBe(
+          undefined
+        ));
+    });
+
+    describe('when nothing found', () => {
+      beforeAll(() => {
+        usersServiceMock.updateById.mockResolvedValueOnce(false);
+      });
+
+      it('should reject', () =>
+        expect(
+          controller.update(userId, userFixture())
+        ).rejects.toThrowErrorMatchingInlineSnapshot(`"User not found"`));
+    });
+  });
+
+  describe('#delete', () => {
+    const userId = 'test-id';
+
+    describe('happy path', () => {
+      it('should resolve', () =>
+        expect(controller.delete(userId)).resolves.toBe(undefined));
+    });
+
+    describe('when nothing found', () => {
+      beforeAll(() => {
+        usersServiceMock.deleteById.mockResolvedValueOnce(false);
+      });
+
+      it('should reject', () =>
+        expect(
+          controller.delete(userId)
+        ).rejects.toThrowErrorMatchingInlineSnapshot(`"User not found"`));
+    });
   });
 });
