fd6681e9e8006331a89b7bcc94f8a1245e2412c5
Eugene Butusov <evgenybutusov@gmail.com>
2023-03-17 15:47:33 +0300

    Intermediate commit

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -5,13 +5,13 @@ import { AuthModule } from '@libs/nest/auth/lib/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import configuration from '../config/default.factory';
+import configurations from '../config/default.factory';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist/interfaces/typeorm-options.interface';
 import { AuthModuleOptions } from '@libs/nest/auth/interfaces/auth-options.interface';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ load: [configuration] }),
+    ConfigModule.forRoot({ load: [...configurations] }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
