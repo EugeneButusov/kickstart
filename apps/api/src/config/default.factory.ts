0d3c374f63acf7e74e371eb3cf759131b3d50ac5
Eugene Butusov <evgenybutusov@gmail.com>
2023-03-17 15:31:14 +0300

    Intermediate commit

--- apps/api/src/config/default.factory.ts
+++ apps/api/src/config/default.factory.ts
@@ -2,20 +2,21 @@ import { AppModule } from '../app/app.module';
 import { UsersModule } from '@libs/nest/users/lib/users.module';
 import { AuthModule } from '@libs/nest/auth/lib/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { toBoolean } from '@libs/shared/utils/to-boolean.util';
 
 export default () => ({
   [AppModule.name]: {
     appName: process.env.APP_NAME || 'Kickstart API',
     port: process.env.APP_PORT,
     host: process.env.APP_HOST,
-    secure: process.env.APP_SECURE || false,
+    secure: toBoolean(process.env.APP_SECURE, false),
     globalPrefix: process.env.APP_GLOBAL_PREFIX || 'api',
   },
   [TypeOrmModule.name]: {
     type: 'mongodb',
     url: process.env.MONGO_URI,
     autoLoadEntities: true,
-    synchronize: process.env.MONGO_SHOULD_SYNCHRONIZE || false,
+    synchronize: toBoolean(process.env.MONGO_SHOULD_SYNCHRONIZE, false),
   },
   [AuthModule.name]: {
     jwt: {
