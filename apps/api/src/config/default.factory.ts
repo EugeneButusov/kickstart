1007b81fc18eba3e7a1775fc17af1c44b6bfac70
Eugene Butusov <evgenybutusov@gmail.com>
2023-03-17 16:01:12 +0300

    Intermediate commit

--- apps/api/src/config/default.factory.ts
+++ apps/api/src/config/default.factory.ts
@@ -1,20 +1,23 @@
 import { AppModule } from '../app/app.module';
-import { AppConfig } from '../interfaces/app-config.interface';
 import { registerAs } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist/interfaces/typeorm-options.interface';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '@libs/nest/auth/lib/auth.module';
 import { AuthModuleOptions } from '@libs/nest/auth/interfaces/auth-options.interface';
 import { toBoolean, toInt } from '@libs/shared/utils';
+import { AppConfig } from '../interfaces/app-config.interface';
 
 export default [
-  registerAs<AppConfig>(AppModule.name, () => ({
-    appName: process.env.APP_NAME || 'Kickstart API',
-    port: toInt(process.env.APP_PORT, 3333),
-    host: process.env.APP_HOST,
-    secure: toBoolean(process.env.APP_SECURE, false),
-    globalPrefix: process.env.APP_GLOBAL_PREFIX || 'api',
-  })),
+  () => ({
+    // AppModule is not initialized yet to be used in `registerAs`
+    [AppModule.name]: {
+      appName: process.env.APP_NAME || 'Kickstart API',
+      port: toInt(process.env.APP_PORT, 3333),
+      host: process.env.APP_HOST,
+      secure: toBoolean(process.env.APP_SECURE, false),
+      globalPrefix: process.env.APP_GLOBAL_PREFIX || 'api',
+    } as AppConfig,
+  }),
 
   registerAs<TypeOrmModuleOptions>(TypeOrmModule.name, () => ({
     type: 'mongodb',
